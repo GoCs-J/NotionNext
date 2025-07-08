668737fe19029aa32eb774bedf49acd795065631
GoCs-J <mulichfe@gmail.com>
2025-07-08 23:37:11 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -43,66 +43,6 @@ export const SideBar = props => {
         </aside>
       )}
 
-      {/* 分类 */}
-      <aside className='w-full rounded shadow overflow-hidden mb-6'>
-        <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-          {locale.COMMON.CATEGORY}
-        </h3>
-
-        <div className='p-4'>
-          <ul className='list-reset leading-normal'>
-            {categoryOptions?.map(category => {
-              return (
-                <Link
-                  key={category.name}
-                  href={`/category/${category.name}`}
-                  passHref
-                  legacyBehavior>
-                  <li>
-                    {' '}
-                    <a
-                      href={`/category/${category.name}`}
-                      className='text-gray-darkest text-sm hover:underline'>
-                      {category.name}({category.count})
-                    </a>
-                  </li>
-                </Link>
-              )
-            })}
-          </ul>
-        </div>
-      </aside>
-
-      {/* 最新文章 */}
-      <aside className='w-full rounded shadow overflow-hidden mb-6'>
-        <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-          {locale.COMMON.LATEST_POSTS}
-        </h3>
-
-        <div className='p-4'>
-          <ul className='list-reset leading-normal'>
-            {latestPosts?.map(p => {
-              return (
-                <Link key={p.id} href={`/${p.slug}`} passHref legacyBehavior>
-                  <li>
-                    {' '}
-                    <a
-                      href={`/${p.slug}`}
-                      className='text-gray-darkest text-sm hover:underline'>
-                      {p.title}
-                    </a>
-                  </li>
-                </Link>
-              )
-            })}
-          </ul>
-        </div>
-      </aside>
-
-      {/* 公告 */}
-      {/* 公告栏 */}
-      {!HIDDEN_NOTIFICATION && <Announcement post={notice} />}
-
       {/* 最近评论 */}
       {COMMENT_WALINE_SERVER_URL && COMMENT_WALINE_RECENT && (
         <aside className='w-full rounded shadow overflow-hidden mb-6'>
