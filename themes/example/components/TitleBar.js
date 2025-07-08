e2a991e7125353f432554183f6bcf38ec6ec3b72
GoCs-J <mulichfe@gmail.com>
2025-07-08 23:30:56 +0800

    Update TitleBar.js

--- themes/example/components/TitleBar.js
+++ themes/example/components/TitleBar.js
@@ -24,23 +24,8 @@ export default function TitleBar(props) {
       {!fullWidth && (
         <div className='relative overflow-hidden text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b'>
           <h1 className='title-1 relative text-xl md:text-4xl pb-4 z-10'>
-            {siteConfig('POST_TITLE_ICON') && (
-              <NotionIcon icon={post?.pageIcon} />
-            )}
             {title}
           </h1>
-          <p className='title-2 relative leading-loose text-gray-dark z-10'>
-            {description}
-          </p>
-          {TITLE_BG && (
-            <>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={headerImage}
-                className='absolute object-cover top-0 left-0 w-full h-full select-none opacity-70 z-0'
-              />
-            </>
-          )}
         </div>
       )}
     </>
