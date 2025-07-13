6b7e442d3941c10259885bd675514e674992d043
GoCs-J <mulichfe@gmail.com>
2025-07-13 22:34:36 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,17 +18,15 @@ const darkModeScript = `
     if (defaultAppearance === 'dark') {
       shouldBeDark = true
     } else if (defaultAppearance === 'auto') {
-      // 检查是否在深色模式时间范围内
       const date = new Date()
       const hours = date.getHours()
       const darkTimeStart = ${BLOG.APPEARANCE_DARK_TIME ? BLOG.APPEARANCE_DARK_TIME[0] : 18}
       const darkTimeEnd = ${BLOG.APPEARANCE_DARK_TIME ? BLOG.APPEARANCE_DARK_TIME[1] : 6}
-      
+
       shouldBeDark = prefersDark || (hours >= darkTimeStart || hours < darkTimeEnd)
     }
   }
-  
-  // 立即设置 html 元素的类
+
   document.documentElement.classList.add(shouldBeDark ? 'dark' : 'light')
 })()
 `
@@ -43,6 +41,9 @@ class MyDocument extends Document {
     return (
       <Html lang={BLOG.LANG}>
         <Head>
+          {/* ✅ 添加 viewport 响应式设置 */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
           {/* 预加载字体 */}
           {BLOG.FONT_AWESOME && (
             <>
