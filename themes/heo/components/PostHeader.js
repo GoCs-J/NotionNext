05dede2ceb250ba9e75fbc23579c43c5f603a46a
GoCs-J <mulichfe@gmail.com>
2025-07-14 20:56:36 +0800

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -23,7 +23,7 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
   const ANALYTICS_BUSUANZI_ENABLE = siteConfig('ANALYTICS_BUSUANZI_ENABLE')
 return (
   <div className='w-full bg-white dark:bg-[#1f1f1f] py-12 px-6 md:px-12 border-b border-gray-200 dark:border-gray-700'>
-    <div className='max-w-4xl mx-auto flex flex-col gap-6'>
+    <div className='max-w-4xl mx-auto flex flex-col gap-2'>
 
       {/* 标题 */}
       <h2 className='text-5xl font-semibold py-10 dark:text-white text-center'>
