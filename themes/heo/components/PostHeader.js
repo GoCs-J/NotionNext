1ebe555b0cb492370a58b8bbd4afc42d991b19c2
GoCs-J <mulichfe@gmail.com>
2025-07-14 20:30:02 +0800

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -51,21 +51,10 @@ return (
                   </div>
                 </Link>
               </>
-            )}
-        {/* 标签 */}
-        {post.tagItems && post.tagItems.length > 0 && (
-          <div className='flex flex-wrap gap-2 items-center'>
+                        )}
+                    {/* 标签 */}
             {post.tagItems.map((tag, index) => (
-              <Link
-                key={index}
-                href={`/tag/${encodeURIComponent(tag.name)}`}
-      className={`cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200
-        mr-2 py-0.5 px-1 text-xs whitespace-nowrap dark:hover:text-white
-         ${selected
-        ? 'text-white dark:text-gray-300 bg-black dark:bg-black dark:hover:bg-gray-900'
-        : `text-gray-600 hover:shadow-xl dark:border-gray-400 notion-{tag.color}_background dark:bg-gray-800`}` }>
-                {tag.name}  
-              </Link>
+              <TagItemMini key={index} tag={tag} />
             ))}
           </div>
         )}
