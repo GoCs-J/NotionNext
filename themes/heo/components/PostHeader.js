ea3dd9196c39fc9781fd1251606a3b1e8668c9fe
GoCs-J <mulichfe@gmail.com>
2025-07-14 18:54:10 +0800

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -1,24 +1,11 @@
-import { HashTag } from '@/components/HeroIcons'
-import LazyImage from '@/components/LazyImage'
-import NotionIcon from '@/components/NotionIcon'
-import WordCount from '@/components/WordCount'
-import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
 import { formatDateFmt } from '@/lib/utils/formatDate'
 import Link from 'next/link'
-import WavesArea from './WavesArea'
 
-/**
- * 文章页头
- * @param {*} param0
- * @returns
- */
-export default function PostHeader({ post, siteInfo, isDarkMode }) {
-  if (!post) {
-    return <></>
-  }
-  // 文章头图
-  const headerImage = post?.pageCover ? post.pageCover : siteInfo?.pageCover
-  const ANALYTICS_BUSUANZI_ENABLE = siteConfig('ANALYTICS_BUSUANZI_ENABLE')
+export const ArticleInfo = props => {
+  const { post } = props
+  const { locale } = useGlobal()
+
   return (
     <section className='w-full mx-auto mb-4'>
       <h2 className='text-5xl font-semibold py-10 dark:text-white text-center'>{post?.title}</h2>
@@ -55,5 +42,5 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
         </div>
       </div>
     </section>
-          )
+  )
 }
