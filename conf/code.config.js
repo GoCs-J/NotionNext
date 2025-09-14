ee113d6561921273e88a9c5106e2c09540a5363a
GoCs-J <mulichfe@gmail.com>
2025-09-14 16:04:06 +1000

    Update code.config.js

--- conf/code.config.js
+++ conf/code.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH ||
-    "https://cdn.jsdelivr.net/gh/PrismJS/prism-themes/themes/prism-darcula.css"
+    'https://cdn.jsdelivr.net/gh/PrismJS/prism-themes/themes/prism-darcula.css',
     // 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-vsc-dark-plus.css', // 代码块默认主题
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || false, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
   PRISM_THEME_LIGHT_PATH:
